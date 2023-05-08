7c2b833b9caa6fe9359af2605a65d22159def7fd
Dylan Tinianov <dylantinianov@gmail.com>
2023-05-08 17:12:15 -0400

    Configure fcl

---
+++ src/app/fcl-verifier.tsx
@@ -0,0 +1,17 @@
+import './config'
+import * as fcl from "@onflow/fcl"
+
+export function SignInButton() {
+  const handleSignIn = () => {
+    fcl.authenticate()
+  }
+
+  return (
+    <button onClick={handleSignIn}>Sign In</button>
+  )
+}
+
+// TODO Sign content
+
+
+// TODO Verify Content
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+'use client';
 import Image from 'next/image'
+import React from 'react';
+import * as verifier from './fcl-verifier'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <verifier.SignInButton/>
+      </div>
+
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -41,20 +48,20 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
+
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Sign In{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+            Log into your wallet
+          </p>       
         </a>
 
         <a
