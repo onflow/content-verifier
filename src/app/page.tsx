5920a228c8d1cf488b7ecb08af8a3d27485813a5
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2023-05-09 16:12:16 -0400

    signing and add hash transaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,12 +36,45 @@ export default function Home() {
     console.log(hashInfo)
 
   }
-  const onSign = (signature: string) => {
-
+  const onSign = async () => {
+    // use fcl to sign hash and return signature
+    try {
+      const MSG = Buffer.from(hash).toString("hex")
+      const [{signature}] = await fcl.currentUser.signUserMessage(MSG)
+      onSigning(hash, signature)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  const onSigning = (signature: string) => {
-    console.log(signature)
+  const onSigning = async (hash: String, signature: String) => {
+    console.log({hash, signature})
+
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import ContentVerifier from 0x9e107eadd013f40e
+
+        transaction (hash: String, signature: String) {
+        
+          let hashTableRef: &ContentVerifier.HashTable
+          let address: Address
+        
+          prepare(signer: AuthAccount) {
+            let contentVerifier = getAccount(0x9e107eadd013f40e)
+            self.address = signer.address
+            self.hashTableRef = contentVerifier.getCapability<&ContentVerifier.HashTable>(/public/hashTable).borrow() 
+              ?? panic("could not borrow reference to HashTable")
+          }
+        
+          execute {
+            self.hashTableRef.addHash(hash: hash, address: self.address, signature: signature);
+          }
+        }
+      `,
+      args: (arg, t) => [arg(hash, t.String), arg(signature, t.String)]
+    })
+
+    fcl.tx(transactionId).subscribe(res => console.log(res.status))
   }
 
   return (
@@ -58,7 +91,7 @@ export default function Home() {
       <div className="relative flex flex-col place-items-center">
        <HashInput onLookup={onLookup} />
        {hash && <DisplayContent hash={hash} />}
-       <SignHash hash={hash} onSigning={onSigning} />
+       <SignHash onSign={onSign} />
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
