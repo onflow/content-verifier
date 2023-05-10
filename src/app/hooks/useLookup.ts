007d249c4137fd0f9f69f502b75ed0c0904255e8
Tom Haile <bradford.haile@gmail.com>
2023-05-10 11:28:44 -0500

    add configure to hooks

--- src/app/hooks/useLookup.ts
+++ src/app/hooks/useLookup.ts
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import * as fcl from "@onflow/fcl";
+import "@/app/config";
 
 export const useLookup = () => {
-  const [hash, setHash] = useState<string | null>(null
+  const [hash, setHash] = useState<string | null>(
+    null
     // "QmQqzMTavQgT4f4T5v6PWBp7XNKtoPmC9jvn12WPT3gkSE"
   );
-  const [isVerified, setIsVerified] = useState<boolean | null>(null)
-  const [ownerAddress, setOwnerAddress] = useState<string | null>(null)
+  const [isVerified, setIsVerified] = useState<boolean | null>(null);
+  const [ownerAddress, setOwnerAddress] = useState<string | null>(null);
 
   const onLookup = async (hash: string) => {
-    setIsVerified(null)
-    setOwnerAddress(null)
+    setIsVerified(null);
+    setOwnerAddress(null);
     setHash(hash);
 
     const hashInfo = await fcl.query({
@@ -31,24 +33,32 @@ export const useLookup = () => {
         }
       `,
       args: (arg: any, t: any) => [arg(hash, t.String)],
-    })
+    });
     if (!hashInfo) {
-      return
+      return;
     }
-    setOwnerAddress(hashInfo.address)
+    setOwnerAddress(hashInfo.address);
 
     const isVerified = await fcl.AppUtils.verifyUserSignatures(
       Buffer.from(hashInfo.hash).toString("hex"),
-      [{f_type: "CompositeSignature", f_vsn: "1.0.0", addr: hashInfo.address, keyId: 1, signature: hashInfo.signature}],
-    )
+      [
+        {
+          f_type: "CompositeSignature",
+          f_vsn: "1.0.0",
+          addr: hashInfo.address,
+          keyId: 1,
+          signature: hashInfo.signature,
+        },
+      ]
+    );
 
-    setIsVerified(isVerified)
-  }
+    setIsVerified(isVerified);
+  };
 
   return {
     isVerified,
     hash,
     ownerAddress,
     onLookup,
-  }
-}
+  };
+};
