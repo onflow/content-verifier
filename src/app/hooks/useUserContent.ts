a62d75fb2360ca4ae21c9da73ca65f58250562fb
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2023-05-11 11:51:11 -0400

    check for address

--- src/app/hooks/useUserContent.ts
+++ src/app/hooks/useUserContent.ts
@@ -9,25 +9,27 @@ export const useUserContent = () => {
     setUserHashes(null)
 
     const user: CurrentUser = await fcl.currentUser.snapshot()
-    const hashes = await fcl.query({
-      cadence:`
-      import ContentVerifier from 0x93585dc5825311aa
-      
-      pub fun main(address: Address): [ContentVerifier.HashInfo]? {
-        let contentVerifier = getAccount(0x93585dc5825311aa)
-      
-        let hashTableCapability = contentVerifier.getCapability<&ContentVerifier.HashTable>(/public/hashTable)
+    if (user.addr) {
+      const hashes = await fcl.query({
+        cadence:`
+        import ContentVerifier from 0x93585dc5825311aa
         
-        let hashTableRef = hashTableCapability.borrow() 
-            ?? panic("could not borrow reference to HashTable capability from user content")
-      
-        return hashTableRef.getHashesForAddress(address: address)
-      }
-      `,
-      args: (arg: any, t: any) => [arg(user.addr, t.Address)],
-    })
+        pub fun main(address: Address): [ContentVerifier.HashInfo]? {
+          let contentVerifier = getAccount(0x93585dc5825311aa)
+        
+          let hashTableCapability = contentVerifier.getCapability<&ContentVerifier.HashTable>(/public/hashTable)
+          
+          let hashTableRef = hashTableCapability.borrow() 
+              ?? panic("could not borrow reference to HashTable capability from user content")
+        
+          return hashTableRef.getHashesForAddress(address: address)
+        }
+        `,
+        args: (arg: any, t: any) => [arg(user.addr, t.Address)],
+      })
 
-    setUserHashes(hashes)
+      setUserHashes(hashes)
+    }
   }
 
   return {
